e90325de871b72394337c31770972ff3963d05c4
LewisStoll <Lewis.Stoll@deutschebahn.com>
2022-06-23 19:57:37 +0200

    added paypal button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -13,6 +13,7 @@ export class HomeComponent implements OnInit {
 
   constructor(private readonly translate: TranslateService) { }
 
+  @ViewChild('paypalRef', { static: true }) private paypalRef: ElementRef
   ngOnInit(): void {
     this.innerWidth = window.innerWidth;
     if (this.innerWidth >= 1000) {
@@ -20,6 +21,41 @@ export class HomeComponent implements OnInit {
     } else if (this.innerWidth < 1000) {
       this.small = true;
       }
+
+      console.log(window.paypal);
+      window.paypal.Buttons(
+        {
+          style: {
+            layout: 'horizontal',
+            color: 'black'
+
+          },
+  
+          createOrder: (data, actions) => {
+            return actions.order.create({
+              purchase_units: [
+                {
+                  amount: {
+                    value: '50',
+                    currency_code: 'EUR'
+                  }
+                }
+              ]
+            });
+          },
+  
+  
+          onApprove: (data, actions) => {
+            return actions.order.capture().then(details => {
+              alert('Zahlung Erfolgreich!');
+            });
+          },
+  
+          onError: error => {
+            console.log(error);
+          }
+        }
+      ).render(this.paypalRef.nativeElement)
   }
   @HostListener('window:resize', ['$event'])
   onResize(event) {
@@ -29,4 +65,5 @@ export class HomeComponent implements OnInit {
       this.small = true;
       }
   }
+
 }
